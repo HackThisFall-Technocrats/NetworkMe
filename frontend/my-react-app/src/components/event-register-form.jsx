b2e7e58e3abd74cfd81786ebc423ba3a0600b30c
Alok Singh <alok15120803@gmail.com>
2024-02-10 04:35:19 +0530

    form updates

--- frontend/my-react-app/src/components/event-register-form.jsx
+++ frontend/my-react-app/src/components/event-register-form.jsx
@@ -51,39 +51,24 @@ const EventRegisterForm = () => {
               type="email"
               id="email"
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="name@flowbite.com"
+              placeholder="name@gmail.com"
             />
             {/* {errors.email && <p>This field is required</p>} */}
           </div>
-          <div>
-            <label
-              htmlFor="subject"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Subject
-            </label>
-            <input
-              {...register("subject", { required: true })}
-              type="text"
-              id="subject"
-              className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="Let us know how we can help you"
-            />
-            {/* {errors.subject && <p>This field is required</p>} */}
-          </div>
+          
           <div className="sm:col-span-2">
             <label
               htmlFor="message"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
             >
-              Your message
+              Description
             </label>
             <textarea
               {...register("message", { required: true })}
               id="message"
               rows={6}
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Leave a comment..."
+              placeholder="Enter details about the event.."
             />
             {/* {errors.message && <p>This field is required</p>} */}
           </div>
