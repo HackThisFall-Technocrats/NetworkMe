05a4c251a83a9ef0a110d0f320b545adce96f2a6
Alok Singh <alok15120803@gmail.com>
2024-02-10 11:38:52 +0530

    form updates

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,12 +1,17 @@
 // import { useState } from 'react'
+import UserCard from './components/UserCard';
 import EventRegisterForm from './components/event-register-form'
+import UserForm from './components/user-form';
+import Testimonials from './components/Testimonials';
 
 function App() {
   return (
     <>
     <div className="flex justify-center flex-col items-center w-[100vw]">
       <h1 className='text-3xl font-bold underline mb-4'>Alok is testing tailwind</h1>
-      <EventRegisterForm />
+      {/* <UserForm /> */}
+      {/* <UserCard /> */}
+      <Testimonials />
       </div>
     </>
   );
---
+++ frontend/my-react-app/src/assets/index.js
@@ -0,0 +1,19 @@
+import random from './random.png'
+import team from './team.png'
+import images from './images.jpg'
+import Logo from './Logo.png'
+import Photo from './Photo.png'
+import innovation from './innovation.png' 
+import productivity from './productivity.png' 
+import Rectangle from './Rectangle.png' 
+import addImage from './addImage.png' 
+import accesibility from './accesibility.png' 
+import social from './social.png' 
+import community from './community.png' 
+import speed from './speed.png' 
+import github from './github.png' 
+import sponsor from './sponsor.png' 
+
+
+export {
+    random , team , images , Logo , Photo , innovation ,   productivity , speed ,   accesibility ,addImage, github, sponsor,  community , social , Rectangle}
