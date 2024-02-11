621fb2552001a6eac0ee6ca732de010a9bdc50ae
Aryan Varma <107306635+aryanvar@users.noreply.github.com>
2024-02-11 05:44:31 +0530

    added routes

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,9 +1,14 @@
+import EventRegisterForm from "./components/EventRegistrationForm";
 import LandingPage from "./pages/LandingPage";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/EventRegistrationForm" element={<EventRegisterForm />} />
+      </Routes>
       <div className="flex justify-center flex-col items-center w-[100vw]">
         {/* <UserForm /> */}
         {/* <UserCard /> */}
